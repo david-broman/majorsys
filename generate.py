2abfb3ec344da7bf926d40832c4cad95ae9ec65e
David Broman <dbro@kth.se>
2023-02-25 11:50:23 +0100

    Extracted numbers

--- generate.py
+++ generate.py
@@ -3,7 +3,43 @@
 import json
 import xmltodict
 
-with open("folkets_sv_en_public.xml") as xml_file:
+# Files
+database = "folkets_sv_en_public.xml"
+html_header = "header.html"
+html_description = "description.html"
+html_footer = "footer.html"
+
+# Mapping of phonetic IPA symbols to numbers. Note that focus is on Swedish sounds.
+pmap = {
+    "s"  : "0",
+    "t"  : "1",
+    "d"  : "1",
+    "n"  : "2",
+    "@"  : "2",
+    "m"  : "3",
+    "r"  : "4",
+    "l"  : "5",
+    "j"  : "6",
+    "$"  : "6",
+    "k"  : "7",
+    "g"  : "7",
+    "f"  : "8",
+    "v"  : "8",
+    "p"  : "9",
+    "b"  : "9"
+}
+
+# Classes of words
+noun = "nn"
+verb = "vb"
+adjective = "jj"
+
+# Extract a sequence of numbers from a phonetic string
+def extract_from_phonetic(phonetic):
+    return ''.join([pmap[x] for x in list(phonetic) if x in pmap])
+
+# Open the free Swedish database with words
+with open(database) as xml_file:
 
     # Read, parse, and convert the lexicon to a dictionary
     print("Parsing XML file...")
@@ -29,11 +65,17 @@ with open("folkets_sv_en_public.xml") as xml_file:
     print_stats("  Total number of adjectives:", "jj", True)
     print_stats("  Number of adjectives with phonetic:", "jj", False)
 
-    # Save words with phonetics in a text file
+    # Extract numbers, classes, and words. Avoid duplicates
+    word_map = {}
     print("Save word file with phonetics...")
     with open("words.txt", "w") as txt_file:
         for w in words:
-            if "@class" in w and "phonetic" in w and w["@class"] == "nn":
+            if "@class" in w and "phonetic" in w:
                 word = w["@value"]
                 phonetic = w["phonetic"]["@value"]
-                txt_file.write(f'\"{word}\", \"{phonetic}\"\n')
+                number = extract_from_phonetic(phonetic)
+                txt_file.write(f'\"{word}\", \"{phonetic}\", {number}\n')
+                word_map[word] = (number, w["@class"])
+
+    # Generate html file
+    print(word_map)
